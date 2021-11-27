acbe9b3dac9025854d0e9b7c5148a9930af13785
Вадим Дмитриев <vadim-cavs@yandex.ru>
2021-11-27 23:05:58 +0300

    added user struct

--- server.go
+++ server.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
+type User struct {
+	Name string `json: "name"`
+	Age  int    `json: "age"`
+}
+
 type service struct {
-	store map[string]string
+	store map[string]*User
+}
+
+func (u *User) toString() string {
+	return fmt.Sprintf("Name: %s and age: %d\n", u.Name, u.Age)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	srv := service{make(map[string]string)}
+	srv := service{make(map[string]*User)}
 	mux.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Write([]byte("I am alive"))
 	})
@@ -29,12 +39,22 @@ func (s *service) create(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
+			return
 		}
-		splittedContent := strings.Split(string(content), " ")
-		s.store[splittedContent[0]] = string(content)
+
+		defer r.Body.Close()
+
+		var u User
+		if err := json.Unmarshal(content, &u); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		s.store[u.Name] = &u
 
 		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte("User was created: " + splittedContent[0]))
+		w.Write([]byte("User was created: " + u.Name))
 		return
 	}
 
@@ -44,8 +64,8 @@ func (s *service) create(w http.ResponseWriter, r *http.Request) {
 func (s *service) getAll(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		response := ""
-		for user := range s.store {
-			response += user + "\n"
+		for _, user := range s.store {
+			response += user.toString()
 		}
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(response))
